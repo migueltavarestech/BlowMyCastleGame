eb71fba67abb405150edc6a8c423d11a4c541f66
Miguel <mike.lo.123@hotmail.com>
2021-02-05 14:59:36 +0000

    Add Packages and Main Class

---
+++ src/org/academiadecodigo/bootcamp55/BlowMyCastleGame/Game/Main.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.bootcamp55.BlowMyCastleGame.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
