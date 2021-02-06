761244308073cc55a8d9309044522c37faab5181
Max Pinheiro <maxnrp@icloud.com>
2021-02-06 16:52:23 +0000

    created the main field ( the grid )

--- src/org/academiadecodigo/bootcamp55/BlowMyCastleGame/Position.java
+++ src/org/academiadecodigo/bootcamp55/BlowMyCastleGame/Position.java
@@ -2,65 +2,65 @@ package org.academiadecodigo.bootcamp55.BlowMyCastleGame;
 
 public class Position {
 
-        private int col;
-        private int row;
+    private int col;
+    private int row;
 
-        public Position(int col, int row) {
-            this.col = col;
-            this.row = row;
-        }
+    public Position(int col, int row) {
+        this.col = col;
+        this.row = row;
+    }
 
-        public void setPos(int col, int row) {
-            this.col = col;
-            this.row = row;
-        }
+    public void setPos(int col, int row) {
+        this.col = col;
+        this.row = row;
+    }
 
-        public int getCol() {
-            return col;
-        }
+    public int getCol() {
+        return col;
+    }
 
-        public int getRow() {
-            return row;
-        }
+    public int getRow() {
+        return row;
+    }
 
-        public void moveInDirection(GridDirection direction, int distance) {
-            switch (direction) {
-                case UP:
-                    moveUp(distance);
-                    break;
-                case DOWN:
-                    moveDown(distance);
-                    break;
-                case LEFT:
-                    moveLeft(distance);
-                    break;
-                case RIGHT:
-                    moveRight(distance);
-                    break;
-            }
+    public void moveInDirection(GridDirection direction, int distance) {
+        switch (direction) {
+            case UP:
+                moveUp(distance);
+                break;
+            case DOWN:
+                moveDown(distance);
+                break;
+            case LEFT:
+                moveLeft(distance);
+                break;
+            case RIGHT:
+                moveRight(distance);
+                break;
         }
+    }
 
-        public void moveUp(int distance) {
-            setPos(getCol(), getRow() - distance);
-        }
+    public void moveUp(int distance) {
+        setPos(getCol(), getRow() - distance);
+    }
 
-        public void moveDown(int distance) {
-            setPos(getCol(), getRow() + distance);
-        }
+    public void moveDown(int distance) {
+        setPos(getCol(), getRow() + distance);
+    }
 
-        public void moveLeft(int distance) {
-            setPos(getCol() - distance, getRow());
-        }
+    public void moveLeft(int distance) {
+        setPos(getCol() - distance, getRow());
+    }
 
-        public void moveRight(int distance) {
-            setPos(getCol()+distance, getRow());
-        }
+    public void moveRight(int distance) {
+        setPos(getCol() + distance, getRow());
+    }
 
-        public String toString() {
-            return "GridPosition{" +
-                    "col=" + col +
-                    ", row=" + row +
-                    "}";
-        }
+    public String toString() {
+        return "GridPosition{" +
+                "col=" + col +
+                ", row=" + row +
+                "}";
+    }
 
 }
