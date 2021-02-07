d12121ea6619327fd5b66b120b15a11e0537c692
Miguel <mike.lo.123@hotmail.com>
2021-02-07 12:23:16 +0000

    Add RPG Maker background

--- src/org/academiadecodigo/bootcamp55/BlowMyCastleGame/Grid.java
+++ src/org/academiadecodigo/bootcamp55/BlowMyCastleGame/Grid.java
@@ -1,16 +1,22 @@
 package org.academiadecodigo.bootcamp55.BlowMyCastleGame;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 import java.awt.*;
 
 public class Grid {
 
     //Player should only be created after the field;
-    public Rectangle field;
+//    public Rectangle field;
+    private Picture canvas;
+    private int PADDING = 10;
 
     public void init() {
-      field = new Rectangle(0, 0, 800, 500);
-      field.draw();
+//      field = new Rectangle(0, 0, 800, 500);
+//      field.draw();
+
+      canvas = new Picture(PADDING, PADDING, "background.png");
+      canvas.draw();
     }
 }
