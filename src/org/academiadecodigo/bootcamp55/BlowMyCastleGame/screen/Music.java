d729620f873db8c1a051fc17f8dc3338e4605042
Miguel <mike.lo.123@hotmail.com>
2021-02-14 20:10:17 +0000

    Include full paths of files for jar build

--- src/org/academiadecodigo/bootcamp55/BlowMyCastleGame/screen/Music.java
+++ src/org/academiadecodigo/bootcamp55/BlowMyCastleGame/screen/Music.java
@@ -3,20 +3,28 @@ package org.academiadecodigo.bootcamp55.BlowMyCastleGame.screen;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class Music{
 
     private Clip clip;
     private File audioFilePath;
+    private static final String soundtrackPath = "/Users/MiguelTavares/BlowMyCastleGame/resources/sounds/soundtrack.wav";
+    private static final String explosionPath = "/Users/MiguelTavares/BlowMyCastleGame/resources/sounds/explosion.wav";
+    private static final String pickUpBombPath = "/Users/MiguelTavares/BlowMyCastleGame/resources/sounds/pick-up-bomb2.wav";
+    private static final String putDownBlockPath = "/Users/MiguelTavares/BlowMyCastleGame/resources/sounds/put-down-block.wav";
 
     public void playMusic() {
-
         //       File sound = new File(path);
         // comment the line above and use the line below when making build, so the path is accessible when executing jar
-        //URL sound = getClass().getClassLoader().getResource(path);
-        File audioFile = new File("resources/sounds/soundtrack.wav");
+        URL soundURL = getClass().getClassLoader().getResource(soundtrackPath);
         try {
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            if(soundURL == null){
+                File audioFile = new File(soundtrackPath);
+                soundURL = audioFile.toURI().toURL();
+            }
+
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
             clip = AudioSystem.getClip();
             clip.open(audioStream);
             start();
@@ -35,8 +43,12 @@ public class Music{
 
     public static void soundPickUpBomb() {
         try {
-            File file = new File("resources/sounds/pick-up-bomb2.wav");
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            URL soundURL2 = Music.class.getClassLoader().getResource(pickUpBombPath);
+            if(soundURL2 == null){
+                File audioFile2 = new File(pickUpBombPath);
+                soundURL2 = audioFile2.toURI().toURL();
+            }
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL2);
             Clip clip2 = AudioSystem.getClip();
             clip2.open(audioStream);
             clip2.start();
@@ -47,8 +59,12 @@ public class Music{
 
     public static void soundBombExplosion() {
         try {
-            File file = new File("resources/sounds/explosion.wav");
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            URL soundURL3 = Music.class.getClassLoader().getResource(explosionPath);
+            if(soundURL3 == null){
+                File audioFile3 = new File(explosionPath);
+                soundURL3 = audioFile3.toURI().toURL();
+            }
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL3);
             Clip clip3 = AudioSystem.getClip();
             clip3.open(audioStream);
             clip3.start();
@@ -59,8 +75,12 @@ public class Music{
 
     public static void soundPutDownBlock() {
         try {
-            File file = new File("resources/sounds/put-down-block.wav");
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            URL soundURL4 = Music.class.getClassLoader().getResource(putDownBlockPath);
+            if(soundURL4 == null){
+                File audioFile4 = new File(putDownBlockPath);
+                soundURL4 = audioFile4.toURI().toURL();
+            }
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL4);
             Clip clip4 = AudioSystem.getClip();
             clip4.open(audioStream);
             clip4.start();
